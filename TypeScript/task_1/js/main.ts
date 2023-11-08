6ee412bb3e80052ccc6d883c6b8707bd62d77566
yerti <yerti0818@gmail.com>
2023-11-08 17:30:24 -0500

    Fix: add interface named Directors that extends Teacher

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,27 +1,45 @@
 // interface named Teacher
 interface Teacher {
-    firstName: string;
-    lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: string | boolean | number;
-};
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: string | boolean | number;
+}
 
+//  interface named Directors that extends Teacher
+// part that asks me to add in task 2
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+
+// Examples
 // Teacher object with the required properties
 const teacher: Teacher = {
-    firstName: 'Manuel',
-    lastName: 'Vidarte',
-    fullTimeEmployee: true,
-    location: 'Lima',
+  firstName: "Manuel",
+  lastName: "Vidarte",
+  fullTimeEmployee: true,
+  location: "Lima",
 };
 const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-  };
+  firstName: "John",
+  fullTimeEmployee: false,
+  lastName: "Doe",
+  location: "London",
+  contract: false,
+};
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
 
 // Add a custom property to the Teacher object
 teacher.contract = true;
