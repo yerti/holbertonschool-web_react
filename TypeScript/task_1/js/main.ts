a0dbf827ff410d73a2ebbff2bed2568156d38c25
yerti <yerti0818@gmail.com>
2023-11-08 18:19:21 -0500

    Fix: add a Class named StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -14,36 +14,50 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-
-// Examples
-// Teacher object with the required properties
-const teacher: Teacher = {
-  firstName: "Manuel",
-  lastName: "Vidarte",
-  fullTimeEmployee: true,
-  location: "Lima",
-};
-const teacher3: Teacher = {
-  firstName: "John",
-  fullTimeEmployee: false,
-  lastName: "Doe",
-  location: "London",
-  contract: false,
+// part of question 3 Printing teachers
+// interface for the function named printTeacherFunction
+interface printTeacherFunction {
+    (firstName: string, lastName:string): string;
 };
 
-const director1: Directors = {
-  firstName: "John",
-  lastName: "Doe",
-  location: "London",
-  fullTimeEmployee: true,
-  numberOfReports: 17,
+// Write a function printTeacher
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    // we get the first letter of the name in capital letters
+    const firstInitial = firstName.charAt(0).toUpperCase();
+
+    // we combine the first letter of the first name with the last name
+    const fullName = `${firstInitial}. ${lastName}`; // "J. Doe"
+
+    return fullName
 };
 
-console.log(director1);
+// part of question 4 where we create a class
+// interface for StudentClass constructor
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+};
 
-// Add a custom property to the Teacher object
-teacher.contract = true;
+// interface for class StudentClass
+interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+};
 
-// Print the Teacher object
-console.log(teacher);
-console.log(teacher3);
+// we implement the StudentClass class
+class StudentClassImpl implements StudentClass {
+    private firstName: string;
+    private lastName: string;
+    
+    constructor(firstName: string , lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+
+    workOnHomework(): string {
+        return "Currently working";
+    };
+
+    displayName(): string {
+        return this.firstName;
+    };
+};
