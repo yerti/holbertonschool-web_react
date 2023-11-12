5da90dccbfaade800f85628605fc96985207dce9
yerti <yerti0818@gmail.com>
2023-11-12 16:45:51 -0500

    Feat: add file of javascript

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src="https://s3.eu-west-3.amazonaws.com/hbtn.intranet/uploads/medias/2019/11/175b85183ecedb529e14.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4MYA5JM5DUTZGMZG%2F20231112%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20231112T203250Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=28eeda9252b88ab0aefc2917d835884ef00c81d193e299748239144047e43010" alt="" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
