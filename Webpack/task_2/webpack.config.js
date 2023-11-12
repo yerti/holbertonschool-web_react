4956a52c4887eeecaed3cf7b67a53d9ca448baae
yerti <yerti0818@gmail.com>
2023-11-12 10:33:41 -0500

    Feat: configuration file

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                    }
+                ]
+            },
+        ],
+    },
+};
