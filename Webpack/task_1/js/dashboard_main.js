0cb2cf20cf20f1dc40b180b2e46d61f554636c21
yerti <yerti0818@gmail.com>
2023-11-12 09:48:04 -0500

    feat: add file

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _, { constant } from 'lodash';
+
+$(document).ready(function() {
+    const number = 0;
+
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to started</button>');
+    $('body').append('<p id="count">1</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    // function count
+    $('button').click(_.debounce(function updateCounter(){
+        number++;
+      $('#count').text(`${number} clicks on the button`);
+    }, 300));
+
+})
