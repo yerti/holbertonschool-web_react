0a6d793077908f08da0c604905337c2f07e1128b
yerti <yerti0818@gmail.com>
2023-11-15 09:02:11 -0500

    Feat: Add question one and two!

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  const focusInput = (inputId) => {
+    const inputElement = document.getElementById(inputId);
+
+    if (inputElement) {
+      inputElement.focus();
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="App">
+        <div className="App-header">
+          <img src="/assets/image/logo-holberton.jpg" alt="" />
+          <h1>School dashboard</h1>
+        </div>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+
+          <div>
+            {/* label and input for email */}
+            <div>
+              <label htmlFor='email' onClick={() => focusInput('email')}>Email:</label>
+              <input type='email' id='email' />
+            </div>
+            {/* label and input for password */}
+            <div>
+              <label htmlFor='password' onClick={() => focusInput('password')}>Password:</label>
+              <input type='password' id='password' />
+            </div>
+
+            <button>Ok</button>
+          </div>
+        </div>
+        <div className="App-footer">
+          <p>Copyright {getFullYear()} - {getFooterCopy('isIndex')}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
