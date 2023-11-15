f7f90eb5f3e20bbe8ec7074e026d310ebe48ab59
yerti <yerti0818@gmail.com>
2023-11-15 08:59:03 -0500

    Fix:add new path to image

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src="https://s3.eu-west-3.amazonaws.com/hbtn.intranet/uploads/medias/2019/11/175b85183ecedb529e14.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4MYA5JM5DUTZGMZG%2F20231112%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20231112T203250Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=28eeda9252b88ab0aefc2917d835884ef00c81d193e299748239144047e43010" alt="" />
+      <img src="/assets/image/logo-holberton.jpg" alt="" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
