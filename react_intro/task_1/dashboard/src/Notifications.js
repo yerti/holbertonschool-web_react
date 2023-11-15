d529e708604c1375c3db467edd91148ce3218bf2
yerti <yerti0818@gmail.com>
2023-11-15 09:04:54 -0500

    Feat: add function notificaciones

---
+++ react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,11 @@
+import './Notifications.css';
+import React from 'react'
+
+
+export default function Notifications() {
+  return (
+    <div className='Notifications'>
+      <p>Here is the list of notifications</p>
+    </div>
+  )
+}
