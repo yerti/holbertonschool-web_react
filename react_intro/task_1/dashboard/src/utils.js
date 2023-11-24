6f1ba05d5b2319a3685c22ad5239c02905bfb318
yerti <yerti0818@gmail.com>
2023-11-24 17:01:46 -0500

    Feat: add function getFullYear and getFooterCopy

---
+++ react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,12 @@
+export function getFullYear() {
+    // obtenemos la fecha actual
+    const currentDate = new Date();
+
+    return currentDate.getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+    const parameter = isIndex ? "Holberton School" : "Holberton School main dashboard";
+
+    return parameter;
+}
