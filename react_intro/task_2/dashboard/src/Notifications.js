e44b00f4711284495f2aa04c87033c8246279db4
yerti <yerti0818@gmail.com>
2023-11-24 17:57:27 -0500

    Feat: add a list the secction notifications

---
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Notifications.css'
+import close from './images/close-icon.png'
+import { getLatestNotification } from './utils'
+
+export default function Notifications() {
+  return (
+    <div className='Notifications'>
+      <button onClick={() => {console.log('Close button has been clicked')}}>
+        <img src={close} alt='icon close'/>
+      </button>
+      <p>Here is the list of notifications</p>
+
+      <ul>
+      <li data-priority="default" style={{color: '#001B79'}}>New course available</li>
+        <li data-priority="urgent" style={{color: 'red'}}>New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} style={{color: 'red'}}></li>
+      </ul>
+    </div>
+  )
+}
