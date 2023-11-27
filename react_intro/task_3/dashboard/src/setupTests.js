8c1c98307969452d2dc4110cd956a531ae2b6aa8
yerti <yerti0818@gmail.com>
2023-11-27 10:50:14 -0500

    Fix: to correct the file

--- react_intro/task_3/dashboard/src/setupTests.js
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,8 @@
-import '@testing-library/jest-dom';
-import Enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
- 
-Enzyme.configure({ adapter: new Adapter() });
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+configure({ adapter: new Adapter() });
+
+chai.use(chaiEnzyme());
