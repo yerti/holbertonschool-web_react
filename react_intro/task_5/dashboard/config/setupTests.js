72a6fe3c8b66618791a1a769082301f982c9a1d2
yerti <yerti0818@gmail.com>
2023-11-28 15:05:28 -0500

    Feat: file configuration

---
+++ react_intro/task_5/dashboard/config/setupTests.js
@@ -0,0 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+configure({ adapter: new Adapter() });
+
+chai.use(chaiEnzyme());
