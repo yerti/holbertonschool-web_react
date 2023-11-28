0ee01004a4f734f8a1563a3ebefe7c437b07ab40
yerti <yerti0818@gmail.com>
2023-11-28 11:28:37 -0500

    Fix: to be able to create the rendering of my App component

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,14 @@
-console.log('app works');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
+import Notifications from './Notifications/Notifications';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div id="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>
+);
