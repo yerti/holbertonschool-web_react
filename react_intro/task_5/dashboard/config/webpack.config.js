7af519351c35c1e3b24942ee07b2fa74590760b2
yerti <yerti0818@gmail.com>
2023-11-29 13:47:21 -0500

    Fix: file of configurtions

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,69 +1,54 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    output : {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist')
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+    hot: true,
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true, // Disable image optimization in development mode
             },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'images',
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65,
-                            },
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4,
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                        },
-                    },
-                ],
-            },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, '../dist')
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        compress: true,
-        hot: true,
-        open: true,
-    } 
-}
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  
+  devtool: 'inline-source-map',
+};
