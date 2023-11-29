dfb7048464b3cc2a605f01c15aab7f098e306527
yerti <yerti0818@gmail.com>
2023-11-29 13:50:33 -0500

    Modify file

--- react_intro/task_5/dashboard/config/setupTests.js
+++ react_intro/task_5/dashboard/config/setupTests.js
@@ -1,6 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import chai, { expect } from 'chai';
+import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 
 configure({ adapter: new Adapter() });
